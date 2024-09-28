107c21123d2866c29c344ca9d2cc2c22e3615bd5
matticall2420 <matticall2420@outlook.jp>
2024-09-28 18:09:41 +0900

    second commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
+
+    public function getByLimit(int $limit_count = 4)
+    {
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
+
+    public function getPaginateByLimit(int $limit_count = 1)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', [PostController::class, 'index']);
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class, 'store']);
